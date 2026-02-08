e8a30a6dfbb8b9d160fa11a703100edeb3daa13d
Omid <omid.joon.2005@gmail.com>
2026-02-08 19:16:14 +0330

    testing if, and, or

--- javascript/script.js
+++ javascript/script.js
@@ -9,11 +9,25 @@ let student = { "age": 15, "name": "ali" }
 
 
 let age = 17;
-let gender = "man";
+let isMale = true;
 
-if ((age > 18) && (gender == "man")) {
+// if (((age > 18 && isMale)) || ((age>21 && !isMale))) {
+//     alert("ok")
+// }
+// else {
+//     alert("not ok")
+// }
+
+let dow = "teusday"
+let clo= 12
+if ((dow =="saturday" || dow == "sunday" || dow == "monday" || dow == "teusday" || dow == "wednesday") && ( clo > 9 && clo < 17))
+{
     alert("ok")
 }
 else {
     alert("not ok")
 }
+
+
+["saturday", "sunday","monday","teusday","wednesday","thursday", "friday"];
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
