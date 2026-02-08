e38622fc9c16668f15c0e754506141addc7183e3
Omid <omid.joon.2005@gmail.com>
2026-02-08 18:45:25 +0330

    learning varriables, operation and data types

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+let num = "ali";
+let num2 = "test2";
+
+let numbers = [1, 2, 4];
+
+
+let student = { "age": 15, "name": "ali" }
+
+
+
+let age = 17;
+let gender = "man";
+
+if ((age > 18) && (gender == "man")) {
+    alert("ok")
+}
+else {
+    alert("not ok")
+}
